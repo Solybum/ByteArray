ac3f3f36a48d3a3bca546b9f430ed50f7ef6def0
Soleil Rojas <soleilrojas@gmail.com>
2018-04-30 06:59:20 -0600

    Move batch script to /build
    Rename so it doesn't loop if it fails to change directory

--- build/nuget-all.bat
+++ build/nuget-all.bat
@@ -1,4 +1,4 @@
-cd src/ByteArray/bin/Release
+cd ../src/ByteArray/bin/Release
 
 del *.nupkg
 
@@ -6,4 +6,4 @@ nuget pack "../../ByteArray.csproj" -Prop Configuration=Release
 
 nuget push "*.nupkg" -Source https://www.nuget.org/api/v2/package
 
-cd ../../../../
+cd ../../../../build
